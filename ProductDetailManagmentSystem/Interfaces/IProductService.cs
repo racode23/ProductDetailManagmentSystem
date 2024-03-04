b264d0bc3e0c1673903e2cc8fb78d66494c50537
RACODE23 <racode977@gmail.com>
2024-03-04 07:58:37 -0800

    Add ProductService

---
+++ ProductDetailManagmentSystem/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using ProductDetailManagmentSystem.Models;
+
+namespace ProductDetailManagmentSystem.Interfaces
+{
+    public interface IProductService
+    {
+        public List<Product> getAllProducts();
+        public Product getProductById(int id);
+
+        public Product AddProduct(Product product);
+
+        public Product UpdateProduct(Product product);
+
+        public void DeleteProduct(int id);
+
+
+    }
+}
---
+++ ProductDetailManagmentSystem/Services/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using ProductDetailManagmentSystem.Interfaces;
+using ProductDetailManagmentSystem.Models;
+
+namespace ProductDetailManagmentSystem.Services
+{
+    public class ProductService : IProductService
+    {
+        public ProductService _productService;
+
+        public ProductService(ProductService productService)
+        {
+           this._productService = productService;
+        }
+        public Product AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> getAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product getProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

4fc3fa9ad1f3d24e585a6901f4a3d6d69c97d75a
RACODE23 <racode977@gmail.com>
2024-03-04 06:32:09 -0800

    Add project files.

---
+++ ProductDetailManagmentSystem/Data/ProductDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductDetailManagmentSystem.Models;
+
+namespace ProductDetailManagmentSystem.Data
+{
+    public class ProductDbContext:DbContext
+    {
+        public ProductDbContext(DbContextOptions options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Product> products { get; set; }
+    }
+}
---
+++ ProductDetailManagmentSystem/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductDetailManagmentSystem.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price{ get; set; }
+
+
+    }
+}

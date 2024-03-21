88890bba144a9b9449072e91003a968b0189dbfe
RACODE23 <racode977@gmail.com>
2024-03-21 08:34:31 -0700

    Autharization and Authentication  Models

---
+++ ProductDetailManagmentSystem/Auth/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductDetailManagmentSystem.Auth
+{
+    public class RegisterModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ ProductDetailManagmentSystem/Auth/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductDetailManagmentSystem.Auth
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "user";
+    }
+}
